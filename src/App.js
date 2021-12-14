ec30de563396834e44c1d7d157d392528e012159
Effie L <Effieland@aol.com>
2021-12-14 16:19:03 +0200

    bla2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="./dana2.jpg" alt="icon" loading="lazy" width="50" height='auto'/>
+
         <p>
           Holaaaa Mi llamo es Dana y yo so de España
 
           Me gusta Propel. Es mi class favorita
         </p>
+
+        
         <a
           className="App-link"
           href="https://reactjs.org"
